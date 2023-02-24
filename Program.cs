f1ced6997529f562e849da43c8a2db6f5a260b07
Khun <lykhun@gmail.com>
2023-02-24 15:29:48 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DemoAdo;
+
+StudentRepository repository = new();
+
+repository.Add(new Student
+{
+    LastName = "LY",
+    FirstName = "Khun",
+    Gender = 2,
+    BirthDate = DateTime.Now,
+    IsGraduated = false,
+});
---
+++ StudentRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAdo
+{
+    public class StudentRepository
+    {
+        private string _connectionString = @"
+            server=(localdb)\MSSQLLocalDB;
+            database=DemoADO;
+            integrated security=true;
+            trusted_connection=true
+        ";
+
+        public List<Student> GetAll()
+        {
+            return null;
+        }
+
+        public Student GetById(int id)
+        {
+            return null;
+        }
+
+        public int Add(Student s)
+        {
+            //1 chercher ou créer une connection
+            using SqlConnection connection = new SqlConnection(_connectionString);
+            //2 creer une commande
+            using SqlCommand command = connection.CreateCommand();
+            //3 créer la requète
+            command.CommandText = @"
+                INSERT INTO Student 
+                (LastName, FirstName, BirthDate, Gender, IsGraduated)
+                OUTPUT INSERTED.Id VALUES
+                (@LastName, @FirstName, @BirthDate, @Gender, @IsGraduated)
+            ";
+            command.Parameters.AddWithValue("LastName", s.LastName);
+            command.Parameters.AddWithValue("FirstName", s.FirstName);
+            command.Parameters.AddWithValue("BirthDate", s.BirthDate);
+            command.Parameters.AddWithValue("Gender", s.Gender);
+            command.Parameters.AddWithValue("IsGraduated", s.IsGraduated);
+            //4 ouvrir la connection
+            connection.Open();
+            //5 exécuter la commande
+            return (int)command.ExecuteScalar();
+        }
+
+        public void Update(Student s) 
+        { 
+        
+        }
+
+        public void Remove(int id) 
+        { 
+
+        }
+    }
+}
